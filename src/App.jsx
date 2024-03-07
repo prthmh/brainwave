061bf81aefb4ad349bfbf1ad7f5e98396defaecb
Prathmesh <111947619+prthmh@users.noreply.github.com>
2024-03-07 21:28:17 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
-      <Header />
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
       <ButtonGradient />
     </>
   );
